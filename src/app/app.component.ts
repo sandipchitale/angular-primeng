61f60a1d748d545e5bcf5eeb0f2dfa886a2c3675
SANDIP CHITALE <sandipchitale@gmail.com>
2023-02-14 20:58:30 -0800

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-primeng';
+export class AppComponent implements OnInit, AfterViewInit {
+  lightTheme = true;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    this.adjustTheme();
+  }
+
+  adjustTheme(event?: any) {
+    let theme = 'light-theme';
+    if (this.lightTheme) {
+      theme = 'light-theme';
+    } else {
+      theme = 'dark-theme';
+    }
+    const themeLink = this.document.getElementById('app-theme') as HTMLLinkElement;
+    if (themeLink) {
+        themeLink.href = theme + '.css';
+    }
+  }
+
 }
